69ef7982092c32f62e310aaf54438a23bf83b1b0
Navya sri G <navyasrigondela45@gmail.com>
2026-04-12 17:29:41 +0530

    Update welcome message to include Jenkins automation

--- src/main/java/GitHubServer.java
+++ src/main/java/GitHubServer.java
@@ -13,7 +13,7 @@ public class GitHubServer extends HttpServlet {
         response.setContentType("text/html");
 
         String html = "<html><body style='text-align:center;margin-top:100px;'>"
-                + "<h1>🚀 Welcome to My DevOps WAR Project</h1>"
+                + "<h1>🚀 Welcome to My DevOps WAR Project automated with Jenkins</h1>"
                 + "<a href='https://github.com/navyasrigondela45'>GitHub</a><br><br>"
                 + "<a href='https://www.linkedin.com/in/navya-sri-gondela-a60091258'>LinkedIn</a>"
                 + "</body></html>";
